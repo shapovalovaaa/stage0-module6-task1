56a7fa405ebabd3c1867ec1b9ea50626d2a703b5
shapovalovaaa <nastyashpovalova70@gmail.com>
2023-08-04 23:56:55 +0300

    feat: solve task

--- src/main/java/com/epam/mjc/stage0/Animal.java
+++ src/main/java/com/epam/mjc/stage0/Animal.java
@@ -1,5 +1,19 @@
 package com.epam.mjc.stage0;
 
 public class Animal {
+    private String color;
+    private int numberOfPaws;
+    private boolean hasFur;
 
+    public Animal(String color, int numberOfPaws, boolean hasFur) {
+        this.color = color;
+        this.numberOfPaws = numberOfPaws;
+        this.hasFur = hasFur;
+    }
+
+    public String getDescription() {
+        String description = "This animal is mostly " + this.color + ". It has " + this.numberOfPaws +
+                (numberOfPaws > 1 ? " paws" : " paw") + " and " + (hasFur ? "a" : "no") + " fur.";
+        return description;
+    }
 }
